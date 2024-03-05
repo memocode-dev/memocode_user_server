46416b0990653b23298527a109d23326bdf1f201
audgks5551 <audgsk5551@daum.net>
2024-03-05 14:46:29 +0900

    update

--- src/main/java/dev/memocode/user_server/security/config/CorsConfig.java
+++ src/main/java/dev/memocode/user_server/security/config/CorsConfig.java
@@ -8,6 +8,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.List;
+import java.util.Set;
 
 @Configuration
 public class CorsConfig {
@@ -15,11 +16,14 @@ public class CorsConfig {
     @Value("${custom.front.domain}")
     private String frontDomain;
 
+    @Value("${custom.front.domain2}")
+    private String frontDomain2;
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of(frontDomain));
+        configuration.setAllowedOrigins(Set.of(frontDomain, frontDomain2).stream().toList());
         configuration.setAllowedMethods(List.of("*"));
         configuration.setAllowedHeaders(List.of("*"));
 
