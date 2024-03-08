adbcb60e25faf8d7620d1a3b3c067f7d49788345
audgks5551 <audgsk5551@daum.net>
2024-03-08 22:24:24 +0900

    [bug] 테스트케이스 실패 해결

--- src/test/java/dev/memocode/user_server/UserServerApplicationTests.java
+++ src/test/java/dev/memocode/user_server/UserServerApplicationTests.java
@@ -1,27 +1,15 @@
 package dev.memocode.user_server;
 
-import dev.memocode.user_server.domain.user.dto.UserCreateDTO;
-import dev.memocode.user_server.usecase.UserUseCase;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-
-import java.util.UUID;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class UserServerApplicationTests {
 
-    @Autowired
-    private UserUseCase userUseCase;
-
     @Test
     void contextLoads() {
-
-        UserCreateDTO dto = UserCreateDTO.builder()
-                .build();
-        UUID user = userUseCase.createUser(dto);
     }
 
 }
