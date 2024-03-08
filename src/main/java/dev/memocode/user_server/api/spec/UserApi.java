e2629ea6ca45d3a5c1946228f47f5b4f5fef06d2
audgks5551 <audgsk5551@daum.net>
2024-03-08 21:34:31 +0900

    [bug] jakarta.validation.ConstraintDeclarationException: HV000151: A method overriding another method must not redefine the parameter constraint configuration, but method UserUseCaseImpl#createUser(UserCreateDTO) redefines the configuration of UserUseCase#createUser(UserCreateDTO). 해결

--- src/main/java/dev/memocode/user_server/api/spec/UserApi.java
+++ src/main/java/dev/memocode/user_server/api/spec/UserApi.java
@@ -5,6 +5,7 @@ import dev.memocode.user_server.form.UserCreateForm;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.jwt.Jwt;
 
@@ -15,7 +16,7 @@ import java.util.List;
 public interface UserApi {
 
     @Operation(summary = "유저 생성(회원가입)")
-    ResponseEntity<Void> createUser(UserCreateForm form, Jwt jwt);
+    ResponseEntity<Void> createUser(@Valid UserCreateForm form, Jwt jwt);
 
     @Operation(summary = "자신의 유저 정보 단건 조회")
     ResponseEntity<UserInfo> userInfo(Jwt jwt);
--- src/main/java/dev/memocode/user_server/usecase/UserUseCase.java
+++ src/main/java/dev/memocode/user_server/usecase/UserUseCase.java
@@ -2,15 +2,17 @@ package dev.memocode.user_server.usecase;
 
 import dev.memocode.user_server.domain.user.dto.UserCreateDTO;
 import dev.memocode.user_server.domain.user.dto.UserInfo;
+import dev.memocode.user_server.domain.user.validation.ValidAccountId;
+import jakarta.validation.Valid;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface UserUseCase {
 
-    UUID createUser(UserCreateDTO dto);
+    UUID createUser(@Valid UserCreateDTO dto);
 
-    UserInfo userInfo(UUID accountId);
+    UserInfo userInfo(@ValidAccountId UUID accountId);
 
     List<UserInfo> findAll();
 }
